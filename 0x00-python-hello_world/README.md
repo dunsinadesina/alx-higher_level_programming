06d2b9d91a052f3320d4e644a54e574a0da35520
dunsinadesina <adesinajesudunsin@gmail.com>
2023-11-27 10:45:05 -0800

    file 0-run

---
+++ 0x00-python-hello_world/README.md
@@ -0,0 +1,1 @@
+first lesson in alx higher level programming python
---
+++ 0x00-python-hello_world/lists.h
@@ -0,0 +1,10 @@
+#ifndef "LISTS_H"
+#define "LISTS_H"
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct lists
+{
+	int i;
+	struct lists *n;
+} lists
+#endif
