0d4e3bd7f9ed5ce4f792a8290ba5e10069ccf319
dunsinadesina <adesinajesudunsin@gmail.com>
2023-12-01 06:04:24 -0800

    ByteCode -> Python #3

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-__import__("os").write(1, "#pythoniscool\n".encode("UTF-8"))
+def magic_calculation(a, b):
+    add, sub = __import__( 'magic_calculation_102', globals(), locals(), ['add', 'sub'].__dict__.values())
+    if a < b:
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+
+            return (c)
+    else:
+        return (sub(a, b))
