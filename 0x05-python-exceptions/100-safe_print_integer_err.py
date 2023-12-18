0821690ef93f0f61110f68e866d491188949462d
dunsinadesina <adesinajesudunsin@gmail.com>
2023-12-18 07:19:09 -0800

    a function that prints an integer.(advanced)

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 import sys
 def safe_print_integer_err(value):
+    """a function that prints an integer.
+    Args:
+        value (int): integer
+    Returns:
+        True or False
+    """
     try:
         print("{:d}".format(value))
         return (True)
