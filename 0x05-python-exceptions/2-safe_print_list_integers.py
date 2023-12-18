21b761f9896c1da9412300511af37af824c8eeeb
dunsinadesina <adesinajesudunsin@gmail.com>
2023-12-18 06:17:41 -0800

    a function that prints x elements of a list

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -9,9 +9,3 @@ def safe_print_list_integers(my_list=[], x=0):
         print("\n")
         return counter
     except IndexError as e:
-        print("Traceback (most recent call last)")
-        print('"File "./2-main.py", line 14, in <module>')
-        print("nb_print = safe_print_list_integers(my_list, len(my_list) + 2)")
-        print('File "/0x05/2-safe_print_list_integers.py", line 7, in safe_print_list_integers print("{:d}".format(my_list[i]), end="")')
-        print("IndexError: list index out of range")
-        print("\n")
