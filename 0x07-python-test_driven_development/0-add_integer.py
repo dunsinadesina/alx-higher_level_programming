554b01231d29c4035a339f94e196d985eac6bb8d
dunsinadesina <adesinajesudunsin@gmail.com>
2024-01-03 05:36:21 -0800

    function that adds two integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -15,11 +15,11 @@ def add_integer(a, b=98):
     """
     if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    elif type(b) not in int, (float):
+    elif type(b) not in (int, float):
         raise TypeError("b must be an integer")
     else:
         return int(a) + int(b)
 
-    if __name__ = "__main__":
+    if __name__ == "__main__":
         import doctest
         doctest.testfile("tests/0-add_integer.txt")
