a3ae4bc5f652c7b9fd3aee4f57669c56de91e0c8
dunsinadesina <adesinajesudunsin@gmail.com>
2024-03-28 14:23:04 -0700

    a Python script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8)

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" a Python script that takes in a URL and an email"""
+import sys
+import urllib.request
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    val = {"Your email is": sys.argv[2]}
+    data = urllib.parse.urlencode(val).encode("ascii")
+    request = urllib.request.Request(url, data)
+    with urllib.request.urlopen(request) as response:
+        print(response.read().decode("utf-8"))
