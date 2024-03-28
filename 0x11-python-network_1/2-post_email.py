67213df7fa6ca6064c44577154aac2b7168b7eb2
dunsinadesina <adesinajesudunsin@gmail.com>
2024-03-28 14:35:28 -0700

    updated

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
 """ a Python script that takes in a URL and an email"""
 import sys
-import urllib.request
 import urllib.parse
+import urllib.request
 
 
 if __name__ == "__main__":
     url = sys.argv[1]
     val = {"Your email is": sys.argv[2]}
     data = urllib.parse.urlencode(val).encode("ascii")
+
     request = urllib.request.Request(url, data)
     with urllib.request.urlopen(request) as response:
         print(response.read().decode("utf-8"))
